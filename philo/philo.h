0a8f85f74ec6218df999aadc003e7d7305c3d09c
luizedua42 <luizedua@student.42sp.org.br>
2023-10-11 15:27:34 -0300

    Initial project setup

---
+++ philo/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 15:19:12 by luizedua          #+#    #+#             */
+/*   Updated: 2023/10/11 15:21:46 by luizedua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+// Includes
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
