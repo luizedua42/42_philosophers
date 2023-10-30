d5eec3e4ba11ff6afb29d6754641e18a5845a1d1
luizedua42 <luizedua@student.42sp.org.br>
2023-10-30 19:02:20 -0300

    starting threads

--- philo/philo.c
+++ philo/philo.c
@@ -6,20 +6,35 @@
 /*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:19:10 by luizedua          #+#    #+#             */
-/*   Updated: 2023/10/30 17:11:15 by luizedua         ###   ########.fr       */
+/*   Updated: 2023/10/30 19:01:46 by luizedua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	print_philos(t_philo *philo, long i);
+
 int	main(int argc, char **argv)
 {
-	t_philo	*philo;
+	t_philo	philo[200];
+	long	i;
+	long	n_of_philos;
 
-	philo = calloc(1, sizeof(t_philo));
+	i = -1;
 	if (argc < 4 || argc > 6)
 		return (EXIT_FAILURE);
-	init_philo(philo, argv);
-	free(philo);
+	if (input_validation(argv[1]) == false)
+		return (EXIT_FAILURE);
+	n_of_philos = ft_atol(argv[1]);
+	while (i < n_of_philos)
+		init_philo(&philo[++i], argv);
 	return (EXIT_SUCCESS);
+}
+
+void	print_philos(t_philo *philo, long i)
+{
+	printf("philo [%ld]die: %ld\n", i, philo->time_to_die);
+	printf("philo [%ld]eat: %ld\n", i, philo->time_to_eat);
+	printf("philo [%ld]sleep: %ld\n", i, philo->time_to_sleep);
+	printf("philo [%ld]times: %ld\n", i, philo->n_of_times);
 }
--- philo/thread_creation.c
+++ philo/thread_creation.c
@@ -1,36 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inits.c                                            :+:      :+:    :+:   */
+/*   thread_creation.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 17:08:39 by luizedua          #+#    #+#             */
-/*   Updated: 2023/10/30 17:33:53 by luizedua         ###   ########.fr       */
+/*   Created: 2023/10/30 17:15:28 by luizedua          #+#    #+#             */
+/*   Updated: 2023/10/30 19:02:02 by luizedua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	init_philo(t_philo *philo, char **av)
+bool	thread_creation(t_philo *philo)
 {
-	size_t	i;
+	pthread_t	thread[200];
 
-	i = 1;
-	while (av[i] != NULL)
-	{
-		if (input_validation(av[i]) == false)
-			return (false);
-		i++;
-	}
-	philo->time_to_die = overflow_validation(ft_atol(av[2]));
-	philo->time_to_eat = overflow_validation(ft_atol(av[3]));
-	philo->time_to_sleep = overflow_validation(ft_atol(av[4]));
-	if (av[5] != NULL)
-	{
-		philo->n_of_times = ft_atol(av[5]);
-	}
-	else
-		philo->n_of_times = LONG_MAX;
 	return (true);
 }
